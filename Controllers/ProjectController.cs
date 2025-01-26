43671e3f2636e631c5480c08a2607bcee8615219
Radia-A <radiaakther.lima@georgebrown.ca>
2025-01-25 22:59:08 -0500

    ICE_1

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
+{
+    /// <summary>
+    /// Index action will retrieve a listening of project's (database)
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var projects = new List<Project>
+        {
+            new Project { ProjectId = 1, Name = "Project 1", Description = "First Project 1" },
+        };
+        return View(projects);
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_ICE.Models;
+
+public class Project
+{
+    /// <summary>
+    /// The unique identifier for the project
+    /// </summary>
+    public int ProjectId { get; set; }
+    
+    /// <summary>
+    /// required field describing a project's name
+    /// </summary>
+    [Required]
+    public required string Name { get; set; }
+    
+    public string? Description { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+    
+    public string? Status { get; set; }
+}
